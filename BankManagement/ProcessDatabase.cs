34397cd7bd4065751ececc5dd8cc4607bc2dbc81
Việt Hưng <hn993533@gmail.com>
2024-11-04 19:11:31 +0700

    doi link sql

--- BankManagement/ProcessDatabase.cs
+++ BankManagement/ProcessDatabase.cs
@@ -10,7 +10,7 @@ namespace BankManagement
 {
     internal class ProcessDatabase
     {
-        public string strConnect = "Data Source=NMC\\SQLEXPRESS;Initial Catalog=QLBanknew;Integrated Security=True;Encrypt=False";
+        public string strConnect = "Data Source=LAPTOP-HUNGVIET\\SQLEXPRESS;Initial Catalog=QLBank;Integrated Security=True;Encrypt=False";
         SqlConnection sqlConnect = null;
 
         private void KetNoiCSDL()
