7c7727047d96e5476ad6b929d8beff4511698229
Cường Nguyễn Mạnh <cuongmanhnguyenz333@gmail.com>
2024-11-15 22:07:23 +0700

    add

--- BankManagement/Program.cs
+++ BankManagement/Program.cs
@@ -13,7 +13,7 @@ namespace BankManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new KhachHang());
+            Application.Run(new DangNhap_DangKi());
             
 
 
